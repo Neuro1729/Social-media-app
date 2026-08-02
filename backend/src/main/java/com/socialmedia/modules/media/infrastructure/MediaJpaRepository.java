3dafaf5ab1247111f4573fb32cd6c9ba93dd9bee
Neuro1729 <aaryanpathak500@gmail.com>
2026-08-02 10:18:47 +0530

    chore: remove app source; keep Docker compose for reuse

--- backend/src/main/java/com/socialmedia/modules/media/infrastructure/MediaJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.socialmedia.modules.media.infrastructure;
-
-import com.socialmedia.modules.media.domain.MediaFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MediaJpaRepository extends JpaRepository<MediaFile, Long> {}
